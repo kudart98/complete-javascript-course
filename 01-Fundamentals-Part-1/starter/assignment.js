c2fef159db644c4cdf37e4003c97ad47f2365143
kudart98 <kudart98@gmail.com>
2023-02-17 21:37:20 -0500

    first lectire finished on data types

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,11 @@
+let country = "USA";
+let continent = "NorthAmerica";
+let population = "Americans";
+
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,4 @@
-let js = 'amaizing';
-if (js === 'amaizing') alert("JavaScript is FUN!What do you think?");
+
 
 let result = 25 + 25 - 100
 console.log(result);
